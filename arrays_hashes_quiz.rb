a84d807903c6a38068498b5865c09f1b774e0ce8
Robert James Bowes <up666724@myport.ac.uk>
2017-08-02 16:51:04 +0100

    Complete array and hash exs

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -9,33 +9,47 @@ puts lines.length()
 puts lines[1]
 
 # 3. How many ways can we return 'Princes Street' from the array?
-puts lines
+puts lines[4]
+puts lines[-1]
+puts lines.last
 
 # 4. Work out the index position of 'Haymarket'
 
+
 # 5. Add 'Airport' to the start of the array
+lines.unshift("Airport")
+puts lines
 
 # 6. Add 'York Place' to the end of the array
+lines.push("York Place")
 
 # 7. Remove 'Edinburgh Park' from the array using it's name
+lines.delete("Edinburgh Park")
 
 # 8. Delete 'Edinburgh Park' from the array by index
+# lines.delete_at(number_here)
 
 # 9. Reverse the positions of the stops in the array
+lines.reverse!
 
 ### B. Given the following data structure:
 
 my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
 
 # 1. How would you return the string `"One"`?
+my_hash[1]
 
 # 2. How would you return the string `"Two"`?
+my_hash[:two]
 
 # 3. How would you return the number `2`?
+my_hash["two"]
 
 # 4. How would you add `{3 => "Three"}` to the hash?
+my_hash[3] = "Three"
 
 # 5. How would you add `{:four => 4}` to the hash?
+my_hash[:four] = 4
 
 ### C. Given the following data structure:
 
