6f78448332f3d5906ec7619a706c2666ac55252d
Robert James Bowes <up666724@myport.ac.uk>
2017-08-02 17:16:46 +0100

    Complete most exercises

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -86,19 +86,42 @@ users = {
 }
 
 # 1. Return Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+puts users["Jonathan"][:twitter]
 
 # 2. Return Erik's hometown
+puts users["Erik"][:home_town]
 
 # 3. Return the array of Erik's favorite numbers
+puts users["Erik"][:favourite_numbers]
 
 # 4. Return the type of Avril's pet Colin
+puts users["Avril"][:pets]["colin"]
 
 # 5. Return the smallest of Erik's favorite numbers
+puts users["Erik"][:favourite_numbers]
 
 # 6. Add the number `7` to Erik's favorite numbers
+eriks_favourite_numbers = users["Erik"][:favourite_numbers]
+eriks_favourite_numbers.push(7)
+puts eriks_favourite_numbers
 
 # 7. Change Erik's hometown to Edinburgh
+users["Erik"][:home_town] = "Edinburgh"
+puts users["Erik"][:home_town]
 
 # 8. Add a pet dog to Erik called "Fluffy"
+eriks_pets = users["Erik"][:pets]["Fluffy"] = :dog
 
 # 9. Add yourself to the users hash
+users[
+  "Robb"] = {
+    :twitter => "12yocali",
+    :favourite_numbers => [4, 7, 14, 17],
+    :home_town => "Guisborough",
+    :pets => {
+      "Gixxa" => :cat,
+      "Bedslug" => :cat
+    }
+  }
+
+  puts users
