aab8662295ec70cdae8ff2da082ff344a1c9c71d
Robert James Bowes <up666724@myport.ac.uk>
2017-08-03 09:52:48 +0100

    Correct after homework runthrough

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -52,77 +52,75 @@
 # # 5. How would you add `{:four => 4}` to the hash?
 # my_hash[:four] = 4
 #
-# ### C. Given the following data structure:
-#
-# users = {
-#   "Jonathan" => {
-#     :twitter => "jonnyt",
-#     :favourite_numbers => [12, 42, 75, 12, 5],
-#     :home_town => "Stirling",
-#     :pets => {
-#       "fluffy" => :cat,
-#       "fido" => :dog,
-#       "spike" => :dog
-#     }
-#   },
-#   "Erik" => {
-#     :twitter => "eriksf",
-#     :favourite_numbers => [8, 12, 24],
-#     :home_town => "Linithgow",
-#     :pets => {
-#       "nemo" => :fish,
-#       "kevin" => :fish,
-#       "spike" => :dog,
-#       "rupert" => :parrot
-#     }
-#   },
-#   "Avril" => {
-#     :twitter => "bridgpally",
-#     :favourite_numbers => [12, 14, 85, 88],
-#     :home_town => "Dunbar",
-#     :pets => {
-#       "colin" => :snake
-#     }
-#   }
-# }
-#
-# # 1. Return Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
-# puts users["Jonathan"][:twitter]
-#
-# # 2. Return Erik's hometown
-# puts users["Erik"][:home_town]
-#
-# # 3. Return the array of Erik's favorite numbers
-# puts users["Erik"][:favourite_numbers]
-#
-# # 4. Return the type of Avril's pet Colin
-# puts users["Avril"][:pets]["colin"]
-#
-# # 5. Return the smallest of Erik's favorite numbers
-# puts users["Erik"][:favourite_numbers]
-#
-# # 6. Add the number `7` to Erik's favorite numbers
-# eriks_favourite_numbers = users["Erik"][:favourite_numbers]
-# eriks_favourite_numbers.push(7)
-# puts eriks_favourite_numbers
-#
-# # 7. Change Erik's hometown to Edinburgh
-# users["Erik"][:home_town] = "Edinburgh"
-# puts users["Erik"][:home_town]
-#
-# # 8. Add a pet dog to Erik called "Fluffy"
-# eriks_pets = users["Erik"][:pets]["Fluffy"] = :dog
-# 
-# # 9. Add yourself to the users hash
-# users[
-#   "Robb"] = {
-#     :twitter => "12yocali",
-#     :favourite_numbers => [4, 7, 14, 17],
-#     :home_town => "Guisborough",
-#     :pets => {
-#       "Gixxa" => :cat,
-#       "Bedslug" => :cat
-#     }
-#   }
-#
-#   puts users
+### C. Given the following data structure:
+
+users = {
+  "Jonathan" => {
+    :twitter => "jonnyt",
+    :favourite_numbers => [12, 42, 75, 12, 5],
+    :home_town => "Stirling",
+    :pets => {
+      "fluffy" => :cat,
+      "fido" => :dog,
+      "spike" => :dog
+    }
+  },
+  "Erik" => {
+    :twitter => "eriksf",
+    :favourite_numbers => [8, 12, 24],
+    :home_town => "Linithgow",
+    :pets => {
+      "nemo" => :fish,
+      "kevin" => :fish,
+      "spike" => :dog,
+      "rupert" => :parrot
+    }
+  },
+  "Avril" => {
+    :twitter => "bridgpally",
+    :favourite_numbers => [12, 14, 85, 88],
+    :home_town => "Dunbar",
+    :pets => {
+      "colin" => :snake
+    }
+  }
+}
+
+# 1. Return Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+puts users["Jonathan"][:twitter]
+
+# 2. Return Erik's hometown
+puts users["Erik"][:home_town]
+
+# 3. Return the array of Erik's favorite numbers
+puts users["Erik"][:favourite_numbers]
+
+# 4. Return the type of Avril's pet Colin
+puts users["Avril"][:pets]["colin"]
+
+# 5. Return the smallest of Erik's favorite numbers
+puts users["Erik"][:favourite_numbers].min
+
+# 6. Add the number `7` to Erik's favorite numbers
+puts users["Erik"][:favourite_numbers].push(7)
+
+# 7. Change Erik's hometown to Edinburgh
+users["Erik"][:home_town] = "Edinburgh"
+puts users["Erik"][:home_town]
+
+# 8. Add a pet dog to Erik called "Fluffy"
+users["Erik"][:pets]["Fluffy"] = :dog
+
+# 9. Add yourself to the users hash
+users[
+  "Robb"] = {
+    :twitter => "12yocali",
+    :favourite_numbers => [4, 7, 14, 17],
+    :home_town => "Guisborough",
+    :pets => {
+      "Gixxa" => :cat,
+      "Bedslug" => :cat
+    }
+  }
+
+  puts users
